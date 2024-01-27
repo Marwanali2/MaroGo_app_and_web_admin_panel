0dd177ef72bc6d571b2f063930c97620324bde89
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-01-28 00:45:24 +0200

    lock device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:adv_ecommerce_app_and_web_admin_panel/adv_ecommerce_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const AdvEcommerceApp());
 }
