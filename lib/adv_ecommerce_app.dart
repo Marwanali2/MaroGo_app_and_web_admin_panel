9e2e110465dfb5b84e1e6b868514a22fbe9e55f3
Marwan Ali Abdelkader <marwanalizz42@gmail.com>
2024-01-27 22:29:31 +0200

    add mvvm design pattern and refactor main.dart

---
+++ lib/adv_ecommerce_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AdvEcommerceApp extends StatelessWidget {
+  const AdvEcommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
